8f01a9e3b154e4337665748f7e356775b066fd8b
silly-boy-lee <2932582804@qq.com>
2017-04-07 19:09:14 +0800

    dmeo

---
+++ src/main/java/com/xykj/zhulingfeng/domain/pojo/User.java
@@ -0,0 +1,39 @@
+package com.xykj.zhulingfeng.domain.pojo;
+
+/**
+ * Created by mr.lee on 2017/4/7.
+ */
+public class User {
+    /**
+     *
+     */
+    private String userId;
+
+    private String userName;
+
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
